d467efbdc37bdb3c5ab0869d9e6ce11c263cb5d6
LilboX3 <lilligruber02@gmail.com>
2024-09-09 19:03:15 +0200

    Tried defining endpoints?

---
+++ DocumentManagementSystem/Controllers/DocumentController.cs
@@ -0,0 +1,48 @@
+﻿using DocumentManagementSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DocumentManagementSystem.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DocumentController : ControllerBase
+    {
+        private readonly ILogger<DocumentController> _logger;
+
+        public DocumentController(ILogger<DocumentController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Document>> GetDocument(long id)
+        {
+            /*var document = await findasync..
+            if (document == null)
+            {
+                return NotFound();
+            }
+            ocument = FIndAsync(name)*/
+            return new Document(id);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostDocument(Document document)
+        {
+            return Ok("document was created");
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDocument(long id, Document document)
+        {
+            return Ok("document was updated");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDocument(long id)
+        {
+            return Ok("document was deleted");
+        }
+
+    }
+}
