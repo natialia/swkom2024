5586f71f1e5fb6ec09ce9bf418a427a675d141ee
LilboX3 <lilligruber02@gmail.com>
2024-12-15 15:23:20 +0100

    get ocr text test

--- DocumentManagementSystem.Tests/Integration/IntegrationTests.cs
+++ DocumentManagementSystem.Tests/Integration/IntegrationTests.cs
@@ -173,5 +173,48 @@ namespace DocumentManagementSystem.Tests.Integration
             Assert.Equal(HttpStatusCode.NoContent, deleteResponse.StatusCode); //returns Ok(response.Documents);
         }
 
+        [Fact]
+        public async Task UploadDocument_GetTextOfDocument_ReturnsOcrText()
+        {
+            //Upload document
+            var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Integration/Vocabulary.pdf");
+            var fileContentBytes = await File.ReadAllBytesAsync(filePath); //read content of pdf
+
+            // make bytearray content out of file
+            var fileContent = new ByteArrayContent(fileContentBytes);
+            fileContent.Headers.ContentType = new MediaTypeHeaderValue("application/pdf");
+
+            // Create MultipartFormDataContent with file, other properties
+            var multipartContent = new MultipartFormDataContent
+                {
+                    // Add file
+                    { fileContent, "UploadedDocument", "Vocabulary.pdf" },
+
+                    //Document properties
+                    { new StringContent("1"), "Id" },
+                    { new StringContent("Vocabulary"), "Name" },
+                    { new StringContent("application/pdf"), "FileType" },
+                    { new StringContent("20kB"), "FileSize" }
+                };
+            var response = await _httpClient.PostAsync("http://localhost:8081/Document", multipartContent);
+
+            var responseBody = await response.Content.ReadAsStringAsync();
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+
+            // ---- Get id of document from response ----
+            var jsonResponse = JsonDocument.Parse(responseBody);
+            var documentId = jsonResponse.RootElement.GetProperty("id").GetInt32();
+
+            //Wait for ocr text to be processed
+            var delay = TimeSpan.FromSeconds(30);
+            await Task.Delay(delay);
+
+            //Get ocr text
+            var textResponse = await _httpClient.GetAsync($"http://localhost:8081/Document/ocrText/{documentId}");
+            var textResponseBody = await textResponse.Content.ReadAsStringAsync();
+            Assert.Equal(HttpStatusCode.OK, textResponse.StatusCode);
+            //Compare if same text as on webui
+        }
+
     }
 }
