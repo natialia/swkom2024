9b876b7e59390a675562544fbe3f9ffb6d995e91
LilboX3 <lilligruber02@gmail.com>
2024-12-15 19:59:31 +0100

    extra comments for documentation

--- dms-api/DTOs/DocumentDTO.cs
+++ dms-api/DTOs/DocumentDTO.cs
@@ -1,11 +1,33 @@
 ﻿namespace DocumentManagementSystem.DTOs
 {
+    /// <summary>
+    /// Represents a document for transfer to the api.
+    /// </summary>
     public class DocumentDTO
     {
+        /// <summary>
+        /// The unique ID of the document.
+        /// </summary>
         public int Id { get; set; }
+
+        /// <summary>
+        /// The name of the document.
+        /// </summary>
         public string? Name { get; set; }
+
+        /// <summary>
+        /// The type of the document (e.g., PDF, Word).
+        /// </summary>
         public string? FileType { get; set; }
+
+        /// <summary>
+        /// The size of the document in bytes.
+        /// </summary>
         public string? FileSize { get; set; }
+
+        /// <summary>
+        /// The extracted OCR Text of the document.
+        /// </summary>
         public string? OcrText { get; set; }
     }
 }
--- dms-bl/Models/Document.cs
+++ dms-bl/Models/Document.cs
@@ -1,11 +1,33 @@
 ﻿namespace dms_bl.Models
 {
+    /// <summary>
+    /// Represents a document for processing in services.
+    /// </summary>
     public class Document
     {
+        /// <summary>
+        /// The unique ID of the document.
+        /// </summary>
         public int Id { get; set; }
+
+        /// <summary>
+        /// The name of the document.
+        /// </summary>
         public string? Name { get; set; }
+
+        /// <summary>
+        /// The type of the document (e.g., PDF, Word).
+        /// </summary>
         public string? FileType { get; set; }
+
+        /// <summary>
+        /// The size of the document in bytes.
+        /// </summary>
         public string? FileSize { get; set; }
+
+        /// <summary>
+        /// The extracted OCR Text of the document.
+        /// </summary>
         public string? OcrText { get; set; }
     }
 }
